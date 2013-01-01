90277f2b2340189ac89d9049ccff0cd4fe37168c
Honza Pokorny <me@honza.ca>
2013-01-01 14:08:53 -0400

    Mutt!

---
+++ mutt/offlineimap_sync.sh
@@ -0,0 +1,19 @@
+#/usr/bin/env bash
+
+PID=`cat ~/.offlineimap/pid`
+ps aux | grep "[ ]$PID" && kill $PID
+
+source /Users/honza/Code/envs/mail/bin/activate
+
+if [ "$1" = "--inbox" ]
+then
+    # Quick sync
+    offlineimap -o -q -f INBOX
+else
+    # Full sync
+    offlineimap -o
+fi
+
+
+
+/usr/local/bin/notmuch new
