2df1594b12148ba0d007e68732ce5d7f91d1381a
Honza Pokorny <me@honza.ca>
2013-03-12 07:32:41 -0300

    MOAR

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Execute this file on a clean machine to create the necessary symlinks and
 # copy required files.
 #
-# Requires git and Dropbox
+# Requires Dropbox
 # This should be run from the default login shell
 
 set -e
@@ -22,12 +22,26 @@ if [ ! -d "$GITHUB" ]; then
     mkdir $GITHUB
 fi
 
+echo "Installing homebrew..."
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+export PATH=/usr/local/bin:$PATH
+
+echo "Installing git..."
+
+echo "Installing all the homebrew things..."
+brew install ack autoconf automake cmake contacts ctags emacs ffmpeg fishfish git gnu-tar gnu-typist gnupg go haskell-platform iftop irssi mercurial mobile-shell msmtp nettle openssl reattach-to-user-namespace ruby tmux tree unrar urlview wget xapian xz
+brew install mutt --with-sidebar-patch
+
+echo "Updating submodules..."
 git submodule init
 git submodule update
 
+echo "Installing Python..."
+brew install python --framework
+
 echo "Installing pip and virtualenv..."
-sudo easy_install pip
-sudo pip install virtualenv virtualenvwrapper
+easy_install pip
+pip install virtualenv virtualenvwrapper fabric pep8
 
 # rc files
 ln -s $DOTFILES/vimrc $HOME/.vimrc
