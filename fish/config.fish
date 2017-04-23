87b7b124166b1358b2f587290164770631851c2b
Honza Pokorny <honza@redhat.com>
2017-04-23 18:18:31 -0300

    Revert "Add fish function for start ssh-agent"
    This reverts commit 40685b956fc068cc231b2d818412aeef9df547a6.

--- fish/config.fish
+++ fish/config.fish
@@ -165,14 +165,3 @@ end
 function move_to_movies
     mv $argv /Volumes/Drive/movies/.
 end
-
-setenv SSH_ENV $HOME/.ssh/environment
-
-function start_agent
-  echo "Initializing new SSH agent ..."
-  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-  echo "succeeded"
-  chmod 600 $SSH_ENV
-  . $SSH_ENV > /dev/null
-  ssh-add
-end
