bcddc782a7ed9ac6d38b524aa07d1b9c3c0a919b
Honza Pokorny <me@honza.ca>
2021-02-24 10:08:22 -0400

    keyboard: i want to be able to type a zero

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   %  |   ^  |   ?  |   /  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * |        |      |      |      |      |  L2  |      |           |      |  L2  |      |    . |      |   =  |        |
+ * |        |      |      |      |      |  L2  |      |           |      |  L2  |   0  |    . |      |   =  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
                 KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-       KC_TRNS, MO(2),   KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+       KC_TRNS, MO(2),   KC_0,    KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
